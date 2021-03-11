77d6b0415348e5957f444895a23b29122f20d81f
edehlol <edymusajev@gmail.com>
2021-03-11 15:56:58 +0200

    added basic logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+// autoclicker
+// 1. price to buy
+// 2. score per second
+
+const grandma = {
+  price: 5,
+  cps: 10,
+};
+
+const AutoClicker = () => {
+  return;
+};
 
 function App() {
+  const clickModifier = 1.15;
+  const costModifier = 1.15;
+  const [score, setScore] = useState(100000);
+  const [cursor, setCursor] = useState({ price: 10, cps: 1 });
+  const [grandma, setGrandma] = useState({ price: 100, cps: 5 });
+
+  const [CPS, setCPS] = useState(0);
+
+  const onManualClick = () => {
+    setScore((score) => score + 1);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const clicks = CPS / 100;
+      setScore((score) => score + clicks);
+    }, 10);
+    return () => clearInterval(interval);
+  }, [CPS]);
+
+  // 1. run every 1 second
+  // 2. add to score clicksPerSecond
+
+  const buyItem = (price) => {
+    const newScore = Math.round(score - price);
+    setScore(newScore);
+  };
+  const upgradeItem = (item, setItem) => {
+    const newCps = Math.round(item.cps * clickModifier);
+    const newPrice = Math.round(item.price * costModifier);
+    setItem({ ...item, cps: newCps, price: newPrice });
+  };
+  const autoClickItem = (item) => {
+    setInterval(() => {
+      setScore((score) => score + item.cps);
+    }, 1000);
+  };
+
+  const onBuyItem = (item, setItem) => {
+    if (score >= item.price) {
+      buyItem(item.price);
+      upgradeItem(item, setItem);
+      // autoClickItem(item);
+      addToCPS();
+      // setAutoClick(autoClick => )
+
+      // 1. set global autoclick number
+      // 2. add up to score from global autoclick number
+    }
+  };
+  const addToCPS = () => {
+    setCPS([cursor, grandma].reduce((total, current) => total.cps + current.cps));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onManualClick}>click</button>
+      <button onClick={() => onBuyItem(cursor, setCursor)}>Cursor: {cursor.price}</button>
+      <button onClick={() => onBuyItem(grandma, setGrandma)}>Grandma: {grandma.price}</button>
+      <p>{Math.round(score)}</p>
+      <p>clicks per second: {CPS}</p>
     </div>
   );
 }
