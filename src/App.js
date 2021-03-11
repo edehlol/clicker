8322f0f71770fb60eff5cd8977b21c4c5ee04319
edehlol <edymusajev@gmail.com>
2021-03-11 16:00:24 +0200

    removed comments and unecessary code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-// autoclicker
-// 1. price to buy
-// 2. score per second
-
-const grandma = {
-  price: 5,
-  cps: 10,
-};
-
-const AutoClicker = () => {
-  return;
-};
-
 function App() {
   const clickModifier = 1.15;
   const costModifier = 1.15;
   const [score, setScore] = useState(100000);
+  const [CPS, setCPS] = useState(0);
   const [cursor, setCursor] = useState({ price: 10, cps: 1 });
   const [grandma, setGrandma] = useState({ price: 100, cps: 5 });
 
-  const [CPS, setCPS] = useState(0);
-
-  const onManualClick = () => {
-    setScore((score) => score + 1);
-  };
   useEffect(() => {
     const interval = setInterval(() => {
       const clicks = CPS / 100;
@@ -33,8 +16,9 @@ function App() {
     return () => clearInterval(interval);
   }, [CPS]);
 
-  // 1. run every 1 second
-  // 2. add to score clicksPerSecond
+  const onManualClick = () => {
+    setScore((score) => score + 1);
+  };
 
   const buyItem = (price) => {
     const newScore = Math.round(score - price);
@@ -45,22 +29,12 @@ function App() {
     const newPrice = Math.round(item.price * costModifier);
     setItem({ ...item, cps: newCps, price: newPrice });
   };
-  const autoClickItem = (item) => {
-    setInterval(() => {
-      setScore((score) => score + item.cps);
-    }, 1000);
-  };
 
   const onBuyItem = (item, setItem) => {
     if (score >= item.price) {
       buyItem(item.price);
       upgradeItem(item, setItem);
-      // autoClickItem(item);
       addToCPS();
-      // setAutoClick(autoClick => )
-
-      // 1. set global autoclick number
-      // 2. add up to score from global autoclick number
     }
   };
   const addToCPS = () => {
